fdf574459ab029858253d61b0ece4ec18a1317c6
Stefano Verna <s.verna@datocms.com>
2024-08-08 15:19:48 +0200

    Update aliases.bash

--- ruby/aliases.bash
+++ ruby/aliases.bash
@@ -1,28 +1,10 @@
 alias b="bundle"
 alias bi="b install"
-alias bo="b open"
-alias bs="b show"
-alias bu="b update"
 
 alias rc="rails console"
 alias rs="rails server -b 0.0.0.0 -p 3001"
-alias rg="rails g"
 
-alias rgco="rg controller"
-alias rgmo="rg model"
-alias rgmi="rg migration"
-alias rgp="rg showcase:presenter"
-
-# to test both up an down migrations
-alias rr="rake routes"
 alias rdm="rake db:migrate db:rollback && rake db:migrate db:test:prepare"
-alias rdp="rake db:test:prepare"
 alias rdmm="rake db:migrate db:test:prepare"
 alias rdr="rake db:rollback"
 alias rds="rake db:seed"
-
-alias devlog="tail -f log/development.log"
-alias fs="foreman start"
-alias fr="foreman run"
-alias zs="zeus start"
-
