b437feed1828ff8166f92bdc69300729f29e9c64
Stefano Verna <s.verna@datocms.com>
2025-04-04 14:56:59 +0200

    Update aliases.bash

--- git/aliases.bash
+++ git/aliases.bash
@@ -1,15 +1,24 @@
+source /etc/bash_completion
+
 alias gf='git fetch --prune'
 alias gff='git fetch --prune --all'
 
 alias gup='git pull-rebase'
+
 alias gm='git merge'
+__git_complete gm _git_merge
 
 alias ga='git add -u'
+__git_complete ga _git_add
+
 alias gaa='git add-remove-all'
 alias gap='git add-interactive'
 
 alias gp='git push'
+__git_complete gp _git_push
+
 alias gpf='git push --force-with-lease'
+__git_complete gpf _git_push
 
 alias gc='git commit -v'
 alias gc!='git commit --amend -v --date="$(date)"'
@@ -19,16 +28,25 @@ alias grh='git reset HEAD --hard'
 alias grmu='git remove-untracked'
 
 alias gco='git checkout'
+__git_complete gco _git_checkout
+
 alias gcm='git checkout master'
 
 alias grb='git rebase'
+__git_complete grb _git_rebase
+
 alias grbi='git rebase-interactive'
+__git_complete gco _git_checkout
+
 alias grbc='git rebase --continue'
 alias grba='git rebase --abort'
 
 alias gb='git branch'
+__git_complete gb _git_branch
 
 alias gs='git status'
+__git_complete gs _git_status
+
 alias gd='git diff --ignore-space-at-eol -b -w --ignore-blank-lines'
 
 function gbd() {
